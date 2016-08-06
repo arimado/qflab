ab7fb17e805f3bf8d7902e817810990b89b5a2ee
Anthony Shaw <anthonyshaw@apache.org>
2016-08-06 16:50:35 +1000

    worked on the icons and layouts

--- mobile/qfapp/www/js/controllers.js
+++ mobile/qfapp/www/js/controllers.js
@@ -17,6 +17,32 @@ angular.module('qff.controllers', [])
   $scope.remove = function(task) {
     todo.remove(task);
   };
+  
+  $scope.share = function() {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your album',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+
+ };
 })
 
 .controller('AccountCtrl', function($scope) {
